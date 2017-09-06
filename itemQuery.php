26135db7fad0cce7d9fc93a845f8149373f042f3
Martin Jay <martinjay180@gmail.com>
2017-09-05 23:11:19 -0400

    refactored itemQuery to be baseQuery and started update functionality

--- itemQuery.php
+++ itemQuery.php
@@ -1,13 +1,20 @@
 <?php
 
-class ItemQueryComparitors {
+class QueryOperations {
+  const Select = 0;
+  const Update = 1;
+  const Insert = 2;
+  const Delete = 3;
+}
+
+class QueryComparitors {
     const Equals = 0;
     const StartsWith = 1;
     const Contains = 2;
     const EndsWith = 3;
 };
 
-class ItemQueryWhere {
+class QueryWhere {
   private $key;
   private $val;
   private $comp;
@@ -20,16 +27,16 @@ class ItemQueryWhere {
 
   function Build(){
     switch($this->comp){
-      case ItemQueryComparitors::Equals:
+      case QueryComparitors::Equals:
         $q = "$this->key = '$this->val'";
         break;
-      case ItemQueryComparitors::Contains:
+      case QueryComparitors::Contains:
         $q = "$this->key LIKE '%$this->val%'";
         break;
-      case ItemQueryComparitors::StartsWith:
+      case QueryComparitors::StartsWith:
         $q = "$this->key LIKE '$this->val%'";
         break;
-      case ItemQueryComparitors::EndsWith:
+      case QueryComparitors::EndsWith:
         $q = "$this->key LIKE '%$this->val'";
         break;
     }
@@ -37,19 +44,26 @@ class ItemQueryWhere {
   }
 };
 
-class ItemQuery {
+class BaseQuery {
 
   public $conn;
   private $query;
   private $debug;
+  private $table;
   private $application;
   public $whereArr = array();
   public $limitArr = array();
   public $orderArr = array();
+  public $setArr = array();
 
-  function __construct($conn, $application = null) {
+  function __construct($conn, $table, $application = null) {
+    $this->table = $table;
     $this->application = $application;
     $this->conn = $conn;
+    $this->debug = $debug;
+  }
+
+  function Debug($debug){
     $this->debug = $debug;
   }
 
@@ -61,13 +75,17 @@ class ItemQuery {
     return $this->Where("template_id", $typeId);
   }
 
+  function ById($id){
+    return $this->Where("id", $id);
+  }
+
   function ByApplication($applicationId){
     return $this->Where("application", $applicationId);
   }
 
   // Query Operations
-  function Where($key, $val, $comp = ItemQueryComparitors::Equals){
-    array_push($this->whereArr, new ItemQueryWhere($key, $val, $comp));
+  function Where($key, $val, $comp = QueryComparitors::Equals){
+    array_push($this->whereArr, new QueryWhere($key, $val, $comp));
     return $this;
   }
 
@@ -84,32 +102,35 @@ class ItemQuery {
   function First(){
     $this->Limit(1, 0);
     $this->Build();
-    return $this->Run()[0];
+    return $this->Run()->rows[0];
   }
 
   function Take($limit, $offset = 0){
     $this->Limit($limit, $offset);
     $this->Build();
-    return $this->Run();
+    return $this->Run()->rows;
   }
 
   function All(){
     $this->Build();
-    return $this->Run();
+    return $this->Run()->rows;
   }
 
   //Build Operations
-  function Build(){
-    if($this->applicaiton != null){
-      $this->ByApplication($this->applicaiton);
+  function Build($operation = QueryOperations::Select){
+    if($this->application != null){
+      $this->ByApplication($this->application);
     }
-    $query = "SELECT * FROM items";
-    if(sizeof($this->whereArr)){
-      $query .= " WHERE ";
-        $query .= join(' AND ', array_map(function($w) {
-          return $w->Build();
-        }, $this->whereArr));
+    switch($operation){
+      case QueryOperations::Select:
+        $query = "SELECT * FROM ".$this->table;
+        break;
+      case QueryOperations::Update:
+        $query = "UPDATE ".$this->table;
+        break;
     }
+    $query .= $this->BuildSet();
+    $query .= $this->BuildWhere();
     if(sizeof($this->orderArr)){
       $query .= " ORDER BY ";
       $query .= join(', ', array_map(function($o) {
@@ -126,12 +147,51 @@ class ItemQuery {
       }
     }
 
-    function Run(){
-      $sql = new sqlQuery($this->conn, $this->query);
+    function BuildSet(){
+      if(sizeof($this->setArr)){
+        $query .= " SET ";
+          $query .= join(' , ', array_map(function($s) {
+            return $s[0]." = '".$s[1]."'";
+          }, $this->setArr));
+      }
+      return $query;
+    }
+
+    function BuildWhere(){
+      if(sizeof($this->whereArr)){
+        $query .= " WHERE ";
+          $query .= join(' AND ', array_map(function($w) {
+            return $w->Build();
+          }, $this->whereArr));
+      }
+      return $query;
+    }
+
+    function Run($queryType = QueryOperations::Select){
+      $sql = new sqlQuery($this->conn, $this->query, $queryType);
       if($this->debug){
         general::pretty($sql);
       }
-      return $sql->rows;
+      $this->Clear();
+      return $sql;
+    }
+
+    function Clear(){
+      $this->whereArr = array();
+      $this->setArr = array();
+      $this->orderArr = array();
+      $this->limitArr = array();
+    }
+
+    //UPDATE Operations
+    function Set($key, $value){
+      array_push($this->setArr, [$key, $value]);
+      return $this;
+    }
+
+    function Update(){
+      $this->Build(QueryOperations::Update);
+      return $this->Run(QueryOperations::Update)->response;
     }
 
   };
