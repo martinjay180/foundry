c48d5a11193eba747f89138f801b5437df6982e0
Martin Jay <martinjay180@gmail.com>
2017-11-06 15:16:27 -0500

    adds String Unit Test

---
+++ tests/StringTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class StringTest extends TestCase
+{
+
+  public function testTruncate(){
+    $this->assertEquals(
+          'truncate...',
+          Strings::Truncate("truncate test", 8)
+      );
+  }
+
+  public function testTruncateNoEllipse(){
+    $this->assertEquals(
+          'truncate',
+          Strings::Truncate("truncate test", 8, false)
+      );
+  }
+}
+?>
